a4dba90c2ea150096553ab308c1a21d6ea50edec
venkat5438 <venkatramreddy108@gmail.com>
2017-03-06 09:46:17 +0530

    removed static infront of By

--- src/main/java/com/delux/common/GmailPage.java
+++ src/main/java/com/delux/common/GmailPage.java
@@ -8,15 +8,15 @@ public class GmailPage{
 	
 	public WebDriver driver;
 	
-	static By UserName				= By.xpath("//input[@id='Email']");
-	static By NxtButtonUname		= By.xpath("//input[@id='next']");
-	static By FindMyAccountLink		= By.xpath("//div[@id='gaia_firstform']/div/a");
-	static By CreateAccountLink		= By.xpath("//span[@id='link-signup']/a");
-	static By PageHeader1			= By.xpath("//div[@class='banner']/h1");
-	static By Pwd	 				= By.xpath("//input[@id='Passwd']");
-	static By SignInBtn				= By.xpath("//input[@id='signIn']");
-	static By ForgotPwd				= By.xpath("//a[@id='link-forgot-passwd']");
-	static By SignInWithDiffAcc		= By.xpath("//span[@id='link-signin-different']/a");
+	 By UserName				= By.xpath("//input[@id='Email']");
+	 By NxtButtonUname		        = By.xpath("//input[@id='next']");
+	 By FindMyAccountLink		        = By.xpath("//div[@id='gaia_firstform']/div/a");
+	 By CreateAccountLink		        = By.xpath("//span[@id='link-signup']/a");
+	 By PageHeader1			        = By.xpath("//div[@class='banner']/h1");
+	 By Pwd	 				= By.xpath("//input[@id='Passwd']");
+	 By SignInBtn				= By.xpath("//input[@id='signIn']");
+	 By ForgotPwd				= By.xpath("//a[@id='link-forgot-passwd']");
+	 By SignInWithDiffAcc		        = By.xpath("//span[@id='link-signin-different']/a");
 	
 	public GmailPage(WebDriver driver){
 		this.driver= driver;
